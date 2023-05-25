f46b4eebe1727ffd952de24d46672007de6a196a
dogcraft <neko@neko.red>
2023-05-25 16:58:40 +0800

    update

--- script/getindex.dart
+++ script/getindex.dart
@@ -1,9 +1,18 @@
 import 'package:dio/dio.dart';
+import 'package:dio_http2_adapter/dio_http2_adapter.dart';
+import 'package:html/parser.dart';
 
-final dio = Dio();
+final dio = Dio()..httpClientAdapter = Http2Adapter(ConnectionManager());
+
 Future<void> main(List<String> args) async {
   print("Hello World");
   final res = await dio.get(
       "https://bbs.dippstar.com/forum.php?forumlist=1&mobile=no&mobile=1&simpletype=no");
-  print(res);
+  // print(res);
+  final document = parse(res.data);
+  final elements = document.getElementsByClassName("box");
+  for (var element in elements) {
+    final rawText = element.text;
+    print("$rawText\n\n");
+  }
 }
