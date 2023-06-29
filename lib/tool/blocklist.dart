8e604dc8d18f44873ef10691fb6c33e52c73dda8
dogcraft <neko@neko.red>
2023-06-29 16:10:42 +0800

    update

---
+++ lib/tool/blocklist.dart
@@ -0,0 +1,5 @@
+class BlockList {
+  static List<String> username = [];
+  static List<int> uid = [];
+  static List<int> tid = [];
+}
--- lib/widget/postthread.dart
+++ lib/widget/postthread.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'dart:io';
+// import 'dart:io';
 import '../tool/replymodel.dart';
 import '../widget/error.dart';
 import '../tool/status.dart';
