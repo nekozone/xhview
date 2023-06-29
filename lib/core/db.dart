fe201eeccc051f9d0f66da3c31f929506c77213c
dogcraft <neko@neko.red>
2023-06-29 22:10:44 +0800

    update

---
+++ lib/core/db.dart
@@ -0,0 +1,38 @@
+import 'package:sqflite/sqflite.dart';
+
+class DB {
+  static Database? db;
+  static const String dbname = 'my.db';
+  static String path = '';
+
+  static Future<void> init() async {
+    if (db != null) {
+      return;
+    }
+
+    try {
+      path = await getDatabasesPath() + dbname;
+      db = await openDatabase(path, version: 1, onCreate: _onCreate);
+    } catch (ex) {
+      // print(ex);
+    }
+  }
+
+  static void _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE blockuser(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        uid INTEGER,
+        name TEXT,
+        time TEXT
+      )
+    ''');
+    await db.execute('''
+      CREATE TABLE blockpost(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        tid INTEGER,
+        time TEXT
+      )
+    ''');
+  }
+}
